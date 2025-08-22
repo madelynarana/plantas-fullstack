453bc6f2724eb49c2bbdd865f5e8e19924a60ccf
Madelyn Arana <madelynarana@gmail.com>
2025-08-22 13:46:44 -0600

    Archivos iniciales.

---
+++ frontend/src/HolaMundo.jsx
@@ -0,0 +1,15 @@
+
+const HolaMundo = () => {
+      return (
+    <div style={{ padding: "20px", border: "2px solid black", borderRadius: "10px" }}>
+      <h1>Hola Mundo</h1>
+      <div style={{ backgroundColor: "blue", width: "100px", height: "100px" }}>
+        Soy un div pequeño
+      </div>
+    </div>
+  );
+  
+}
+
+export default HolaMundo;
+
---
+++ frontend/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HolaMundo from "./HolaMundo";
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HolaMundo />
+  </React.StrictMode>
+);
