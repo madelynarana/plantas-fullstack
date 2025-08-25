eccf8fe1c003cbc8ee57113276e07d49f2923550
Madelyn Arana <madelynarana@gmail.com>
2025-08-25 12:44:02 -0600

    Añadir gráfica

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import EditarPlanta from './plantaEditarPorId';
 import BuscarPlanta from './plantaBuscar';
 import PlantasPorFamilia from './plantaPorFamilia';
+import ApiGrafica from './apiGrafica';
 
 
 export const App = () => {
@@ -14,6 +15,7 @@ export const App = () => {
                 <Route path='/plantasPorFamilia' element={<PlantasPorFamilia />} />            
                 <Route path='/' element={<BuscarPlanta />} />
                 <Route path='/plantaEditarPorId/:idPlant' element={<EditarPlanta />} />
+                <Route path='/apiGrafica' element={<ApiGrafica/>} />
             </Routes>
         </div>
     );
---
+++ frontend/src/apiGrafica.jsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react';
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, } from 'chart.js';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+
+const ApiGrafica = () => {
+    const [datosApi, setDatosApi] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const API_KEY = 'n6lhC0Ed6H7Drk8x0IZnIWwj2n9HyOi3QhnTS3PyHVpVfO52hV';
+
+    useEffect(() => {
+        const estadistica = async () => {
+            try {
+                const response = await fetch('https://plant.id/api/v3/usage_info', {
+                    headers: { 'Api-Key': API_KEY },
+                });
+                const data = await response.json();
+                setDatosApi(data);
+            } catch (error) {
+                console.error('Error al obtener datos:', error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        estadistica();
+    }, []);
+
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Total de registros por estado',
+            },
+        },
+    };
+
+    if (loading) return <p>Cargando datos de la API...</p>;
+    if (!datosApi) return <p>Error al cargar los datos de la API.</p>;
+
+    const datosGraficas = {
+        labels: ['Total', 'Usados', 'Restantes'],
+        datasets: [
+            {
+                label: 'Créditos API',
+                data: [
+                    datosApi.credit_limits?.total || 0,
+                    datosApi.used?.total || 0,
+                    datosApi.remaining?.total || 0,
+                ],
+                backgroundColor: ['#b5963fff', '#3ba2c2ff', '#4caf50'],
+            },
+        ],
+    };
+
+    return (
+        <div>
+            <h1>Mi Gráfica de Barras</h1>
+
+            <p>
+                <b>Estatus de API:</b> {datosApi.active ? 'Activo' : 'Inactivo'} <br />
+            </p>
+
+            <div style={{ width: '400px', height: '400px' }}>
+                <Bar data={datosGraficas} options={options} />
+            </div>
+        </div>
+    );
+};
+
+export default ApiGrafica;
